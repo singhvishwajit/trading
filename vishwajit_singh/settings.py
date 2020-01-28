0ffd2e5a6cc81f3ef5e36beffd7c83e1052293bd
Vishwajit Singh <vishwajit7@gmail.com>
2020-01-28 19:27:35 +0530

    Updated settigns

--- vishwajit_singh/settings.py
+++ vishwajit_singh/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pc!&wk*sw#ax=@_nl-a=2pc&h4*geez_=77svne7zc_3jo@&@y'
+# SECRET_KEY = 'pc!&wk*sw#ax=@_nl-a=2pc&h4*geez_=77svne7zc_3jo@&@y'
+
+SECRET_KEY = os.environ.get('SECRET_KEY_VS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vishwajitsingh.herokuapp.com']
 
 
 # Application definition
